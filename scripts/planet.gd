6e3089ce2507b8385ca22cf91c9b7ae2826b9274
Nicholas Casto <nicholascasto99@gmail.com>
2026-02-22 17:08:12 -0800

    oh yeah

---
+++ scripts/planet.gd
@@ -0,0 +1,12 @@
+extends Node3D
+
+@onready var noise: FastNoiseLite = $MeshInstance3D.material_overlay.albedo_texture.noise
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	noise.offset.z += delta * 2.0
